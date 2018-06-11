6f71de7fa7527c4d86c011594d93e935afe8c652
Mautsch, Andreas <andreas.mautsch@cgm.com>
2018-06-11 17:22:44 +0200

    CountryService added

--- src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryService.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryService.java
@@ -3,6 +3,7 @@ package org.goafabric.spring.boot.exampleservice.service;
 import org.goafabric.spring.boot.exampleservice.service.dto.Country;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -16,4 +17,16 @@ public interface CountryService {
 
     @RequestMapping(method = RequestMethod.GET)
     List<Country> getAllCountries();
+
+
+    /*
+    public Country findByIsoCode(@NonNull final String isoCode) {
+    public Country findByName(@NonNull final String name) {
+    */
+
+    @RequestMapping(method = RequestMethod.POST)
+    void save(Country country);
+
+    @RequestMapping(method = RequestMethod.DELETE)
+    void delete(@RequestParam("id") String id);
 }
