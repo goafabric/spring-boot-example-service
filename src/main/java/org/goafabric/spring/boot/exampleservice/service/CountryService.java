251f2d8477b1aed1bb75efe793e00d19090226e9
Mautsch, Andreas <andreas.mautsch@cgm.com>
2018-06-11 17:15:21 +0200

    CountryService added

--- src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryService.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryService.java
@@ -1,26 +1,19 @@
 package org.goafabric.spring.boot.exampleservice.service;
 
 import org.goafabric.spring.boot.exampleservice.service.dto.Country;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 import java.util.List;
 
 /**
  * Created by andreas.mautsch on 08.06.2018.
  */
 
-@Path(CountryService.RESOURCE)
-@Consumes(MediaType.APPLICATION_JSON)
-@Produces(MediaType.APPLICATION_JSON)
+@RequestMapping(CountryService.RESOURCE)
 public interface CountryService {
     String RESOURCE = "/countries";
 
-    @Path("/")
-    //@Path("{orderId}")
-    @GET
+    @RequestMapping(method = RequestMethod.GET)
     List<Country> getAllCountries();
 }
--- src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryServiceBean.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryServiceBean.java
@@ -3,10 +3,13 @@ package org.goafabric.spring.boot.exampleservice.service;
 import lombok.experimental.Delegate;
 import org.goafabric.spring.boot.exampleservice.logic.CountryLogicBean;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by andreas.mautsch on 08.06.2018.
  */
+
+@RestController
 public class CountryServiceBean implements CountryService {
     @Delegate
     @Autowired
