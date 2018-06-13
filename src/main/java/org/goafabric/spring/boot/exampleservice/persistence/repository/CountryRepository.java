07c4bedb4c4236b599d57d40bb66422400247946
Mautsch, Andreas <andreas.mautsch@cgm.com>
2018-06-13 17:24:18 +0200

    Upgrade to Java 1.10

--- src/main/java/org/goafabric/spring/boot/exampleservice/persistence/repository/CountryRepository.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/persistence/repository/CountryRepository.java
@@ -1,6 +1,9 @@
 package org.goafabric.spring.boot.exampleservice.persistence.repository;
 
+import org.goafabric.spring.boot.exampleservice.configuration.CacheConfiguration;
 import org.goafabric.spring.boot.exampleservice.persistence.domain.CountryBO;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -8,8 +11,8 @@ import org.springframework.data.repository.query.Param;
 /**
  * Created by andreas.mautsch on 11.06.2018.
  */
-//@CacheConfig(cacheNames = {CacheConfiguration.PERSISENCE})
-//@Cacheable
+@CacheConfig(cacheNames = {CacheConfiguration.PERSISENCE})
+@Cacheable
 public interface CountryRepository extends JpaRepository<CountryBO, String> {
     CountryBO findByIsoCode(String isoCode);
 
