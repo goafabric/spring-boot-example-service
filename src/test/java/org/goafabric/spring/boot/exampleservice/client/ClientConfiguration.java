a4f9b446bfbb24458b80e9e3cee7fc55edc1f953
Mautsch, Andreas <andreas.mautsch@cgm.com>
2019-02-25 09:39:16 +0100

    rest fixed

--- src/test/java/org/goafabric/spring/boot/exampleservice/client/ClientConfiguration.java
+++ src/test/java/org/goafabric/spring/boot/exampleservice/client/ClientConfiguration.java
@@ -1,14 +1,25 @@
 package org.goafabric.spring.boot.exampleservice.client;
 
+import java.util.Arrays;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ClientConfiguration {
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter()));
+        restTemplate.getInterceptors().add((request, body, execution) -> {
+            request.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
+            return execution.execute(request, body);
+        });
+
+        return restTemplate;
     }
 
     /*
