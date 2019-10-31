79c5d2aa048e6969f33fcc45eb980117df0344ea
Mautsch, Andreas <andreas.mautsch@cgm.com>
2019-10-31 14:30:59 +0100

    refactoring

--- src/test/java/org/goafabric/spring/boot/exampleservice/client/ClientConfiguration.java
+++ src/test/java/org/goafabric/spring/boot/exampleservice/client/ClientConfiguration.java
@@ -1,14 +1,13 @@
 package org.goafabric.spring.boot.exampleservice.client;
 
-import java.util.Arrays;
-
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
 
 @Configuration
 public class ClientConfiguration {
