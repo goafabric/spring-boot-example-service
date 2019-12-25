687c462e02f41ab2e00f48a2347646d7f27a93fd
goafabric <amautsch@gmx.de>
2019-12-25 18:08:17 +0100

    security added

--- src/main/java/org/goafabric/spring/boot/exampleservice/Application.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/Application.java
@@ -1,13 +1,15 @@
 package org.goafabric.spring.boot.exampleservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.ManagementWebSecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * Created by amautsch on 26.06.2015.
  */
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class, ManagementWebSecurityAutoConfiguration.class })
 public class Application {
 
     public static void main(String[] args){
---
+++ src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SecurityConfiguration.java
@@ -0,0 +1,51 @@
+
+package org.goafabric.spring.boot.exampleservice.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+@ConditionalOnProperty(value = "security.enabled", matchIfMissing = true)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public static NoOpPasswordEncoder passwordEncoder() {
+        return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin")
+                .roles("STANDARD_READ_ROLE", "STANDARD_WRITE_ROLE");
+    }
+
+    @Override
+    protected void configure(final HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests()
+                //.requestMatchers(EndpointRequest.to("actuator", "welcome")).permitAll()
+                .antMatchers(
+                        "/actuator/**",
+                        "/", "/welcome/**"
+                ).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+}
