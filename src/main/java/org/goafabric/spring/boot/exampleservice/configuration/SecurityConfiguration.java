5b46ad34831dbc683a6654b4dbd9b55a6c54d312
goafabric <amautsch@gmx.de>
2019-12-25 19:51:59 +0100

    security added

--- src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SecurityConfiguration.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SecurityConfiguration.java
@@ -3,6 +3,7 @@ package org.goafabric.spring.boot.exampleservice.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -10,6 +11,8 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @Configuration
@@ -26,13 +29,9 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception {
-
-        /*
-        final String password = PasswordEncoderFactories
-                .createDelegatingPasswordEncoder()
-                .encode("admin");
-        */
+        //System.out.println(passwordEncoder().encode("admin"));
         auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
                 .withUser("admin")
                 .password(password)
                 .roles("STANDARD_ROLE");
@@ -55,4 +54,8 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
