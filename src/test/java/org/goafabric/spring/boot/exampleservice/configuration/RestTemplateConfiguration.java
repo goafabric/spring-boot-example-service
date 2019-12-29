57d7e572ca43e72649d0dad236c8e20d4e7399fc
goafabric <amautsch@gmx.de>
2019-12-29 17:17:49 +0100

    security added

--- src/test/java/org/goafabric/spring/boot/exampleservice/configuration/RestTemplateConfiguration.java
+++ src/test/java/org/goafabric/spring/boot/exampleservice/configuration/RestTemplateConfiguration.java
@@ -1,7 +1,9 @@
 package org.goafabric.spring.boot.exampleservice.configuration;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -10,14 +12,21 @@ import org.springframework.web.client.RestTemplate;
 import java.util.Arrays;
 
 @Configuration
+@PropertySource("test.properties")
 public class RestTemplateConfiguration {
+    @Value("${credentials.user}")
+    private String user;
+
+    @Value("${credentials.password}")
+    private String password;
+
     @Bean
     public RestTemplate restTemplate() {
         final RestTemplate restTemplate = new RestTemplate();
         restTemplate.setMessageConverters(Arrays.asList(new MappingJackson2HttpMessageConverter()));
         restTemplate.getInterceptors().add((request, body, execution) -> {
             request.getHeaders().set(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-            request.getHeaders().setBasicAuth("admin", "admin");
+            request.getHeaders().setBasicAuth(user, password);
             return execution.execute(request, body);
         });
 
