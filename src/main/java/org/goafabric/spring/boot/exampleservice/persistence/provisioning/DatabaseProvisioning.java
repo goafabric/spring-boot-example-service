02d86fcbeddb43ba3ffce68768756f647a0c0093
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-04-07 14:46:45 +0200

    bugfixes

--- src/main/java/org/goafabric/spring/boot/exampleservice/persistence/provisioning/DatabaseProvisioning.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/persistence/provisioning/DatabaseProvisioning.java
@@ -2,6 +2,7 @@ package org.goafabric.spring.boot.exampleservice.persistence.provisioning;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.flywaydb.core.Flyway;
 import org.h2.util.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,43 +14,25 @@ import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
-@AllArgsConstructor
 public class DatabaseProvisioning implements CommandLineRunner {
 
     @Autowired
     private ApplicationContext  applicationContext;
 
-    @Autowired
+    @Autowired(required = false)
     private DatabaseImport      databaseImport;
 
     @Bean
     public FlywayMigrationStrategy flywayMigrationStrategy() {
         return flyway -> {
-            final String goals = getGoals();
-            if (goals.contains("-migrate")) {
-                log.info("calling flyway migration");
-                flyway.migrate();
-            }
-
-            if (goals.contains("-clean")) {
-                log.info("calling flyway clean");
-                flyway.clean();
-            }
+            doFlyway(flyway);
         };
     }
 
     @Override
     public void run(String... args) throws Exception {
         final String goals = getGoals();
-        if (goals.contains("-import-catalog-data")) {
-            log.info("calling catalog data import");
-            databaseImport.importCatalogData();
-        }
-
-        if (goals.contains("-import-demo-data")) {
-            log.info("calling demo data import");
-            databaseImport.importDemoData();
-        }
+        doImport(goals);
 
         if (goals.contains("-terminate")) {
             log.info("terminating app");
@@ -57,10 +40,37 @@ public class DatabaseProvisioning implements CommandLineRunner {
         }
     }
 
+    private void doFlyway(Flyway flyway) {
+        final String goals = getGoals();
+        if (goals.contains("-migrate")) {
+            log.info("calling flyway migration");
+            flyway.migrate();
+        }
+
+        if (goals.contains("-clean")) {
+            log.info("calling flyway clean");
+            flyway.clean();
+        }
+    }
+
+    private void doImport(String goals) {
+        if (databaseImport != null) {
+            if (goals.contains("-import-catalog-data")) {
+                log.info("calling catalog data import");
+                databaseImport.importCatalogData();
+            }
+
+            if (goals.contains("-import-demo-data")) {
+                log.info("calling demo data import");
+                databaseImport.importDemoData();
+            }
+        }
+    }
+
     private String getGoals() {
         final String goals = applicationContext.getEnvironment().getProperty("database.provisioning.goals");
         return (StringUtils.isNullOrEmpty(goals) == true)
-                ? "-dddf" : goals;
+                ? "-migrate" : goals;
     }
 
     public void initiateShutdown(int exitCode){
