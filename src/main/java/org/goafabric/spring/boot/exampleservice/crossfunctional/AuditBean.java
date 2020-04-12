76ba8337ac7115d1eab43b422468f91670d89c44
goafabric <amautsch@gmx.de>
2020-04-12 10:45:07 +0200

    Audit Listener added

---
+++ src/main/java/org/goafabric/spring/boot/exampleservice/crossfunctional/AuditBean.java
@@ -0,0 +1,102 @@
+package org.goafabric.spring.boot.exampleservice.crossfunctional;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.goafabric.spring.boot.exampleservice.logic.CountryLogic;
+import org.joda.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.PostPersist;
+import javax.persistence.PostRemove;
+import javax.persistence.PostUpdate;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class AuditBean {
+    @Autowired
+    private CountryLogic countryLogic;
+
+    private enum DbOperation {
+        INSERT,
+        UPDATE,
+        DELETE
+    }
+
+    @Data
+    @Builder
+    private static class AuditEvent {
+        private String type;
+        private DbOperation operation;
+        private String createdBy;
+        private Date createdAt;
+        private String modifiedBy;
+        private Date   modifiedAt;
+        private String value;
+    }
+
+    @PostPersist
+    public void postPersist(Object person) {
+        logAudit(person, DbOperation.INSERT);
+    }
+
+    @PostUpdate
+    public void postUpdate(Object person) {
+        logAudit(person, DbOperation.UPDATE);
+    }
+
+    @PostRemove
+    public void postRemove(Object person) {
+        logAudit(person, DbOperation.DELETE);
+    }
+
+    private void logAudit(final Object object, final DbOperation operation) {
+        try {
+            final String value =
+                    new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(object);
+
+            final AuditEvent auditEvent;
+            if (operation == DbOperation.INSERT) {
+                auditEvent = createInsertEvent(object, operation, value);
+            } else {
+                auditEvent = createUpdateDeleteEvent(object, operation, value);
+            }
+
+            log.info("New audit event : {}", auditEvent.toString());
+        } catch (Exception e) {
+            log.error("Error during audit: {}", e.getMessage(), e);
+        }
+    }
+
+    private AuditEvent createUpdateDeleteEvent(
+            final Object object, final DbOperation operation, final  String value) {
+        return AuditEvent.builder()
+                .operation(operation)
+                .type(object.getClass().getSimpleName())
+                .modifiedBy(getUserName())
+                .modifiedAt(LocalDateTime.now().toDate())
+                .value(value)
+                .build();
+    }
+
+    private AuditEvent createInsertEvent(
+            final Object object, final DbOperation operation, final String value) {
+        return AuditEvent.builder()
+                .operation(operation)
+                .type(object.getClass().getSimpleName())
+                .createdBy(getUserName())
+                .createdAt(LocalDateTime.now().toDate())
+                .value(value)
+                .build();
+    }
+
+    private String getUserName() {
+        final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return (authentication == null) ? "" : authentication.getName();
+    }
+}
--- src/main/java/org/goafabric/spring/boot/exampleservice/crossfunctional/AuditListener.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/crossfunctional/AuditListener.java
@@ -1,11 +1,6 @@
 package org.goafabric.spring.boot.exampleservice.crossfunctional;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Builder;
-import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.goafabric.spring.boot.exampleservice.logic.CountryLogic;
-import org.joda.time.LocalDateTime;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -14,7 +9,6 @@ import org.springframework.stereotype.Service;
 import javax.persistence.PostPersist;
 import javax.persistence.PostRemove;
 import javax.persistence.PostUpdate;
-import java.util.Date;
 
 /**
  * Created by andreas.mautsch on 31.01.2018.
@@ -34,81 +28,19 @@ public class AuditListener {
         }
     }
 
-    private enum DbOperation {
-        INSERT,
-        UPDATE,
-        DELETE
-    }
-
-    @Data
-    @Builder
-    private static class AuditEvent {
-        private String type;
-        private DbOperation operation;
-        private String createdBy;
-        private Date   createdAt;
-        private String modifiedBy;
-        private Date   modifiedAt;
-        private String value;
-    }
-
     @PostPersist
     public void postPersist(Object person) {
-        logAudit(person, DbOperation.INSERT);
+        BeanUtil.getBean(AuditBean.class).postPersist(person);
     }
 
     @PostUpdate
     public void postUpdate(Object person) {
-        logAudit(person, DbOperation.UPDATE);
+        BeanUtil.getBean(AuditBean.class).postUpdate(person);
     }
 
     @PostRemove
     public void postRemove(Object person) {
-        logAudit(person, DbOperation.DELETE);
+        BeanUtil.getBean(AuditBean.class).postRemove(person);
     }
 
-    private void logAudit(final Object object, final DbOperation operation) {
-        try {
-            final String value =
-                    new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(object);
-
-            final AuditEvent auditEvent;
-            if (operation == DbOperation.INSERT) {
-                auditEvent = createInsertEvent(object, operation, value);
-            } else {
-                auditEvent = createUpdateDeleteEvent(object, operation, value);
-            }
-
-            CountryLogic logic = BeanUtil.getBean(CountryLogic.class);
-            log.info("New audit event : {}", auditEvent.toString());
-        } catch (Exception e) {
-            log.error("Error during audit: {}", e.getMessage(), e);
-        }
-    }
-
-    private AuditEvent createUpdateDeleteEvent(
-            final Object object, final DbOperation operation,final  String value) {
-        return AuditEvent.builder()
-                .operation(operation)
-                .type(object.getClass().getSimpleName())
-                .modifiedBy(getUserLogin())
-                .modifiedAt(LocalDateTime.now().toDate())
-                .value(value)
-                .build();
-    }
-
-    private AuditEvent createInsertEvent(
-            final Object object, final DbOperation operation, final String value) {
-        return AuditEvent.builder()
-                .operation(operation)
-                .type(object.getClass().getSimpleName())
-                .createdBy(getUserLogin())
-                .createdAt(LocalDateTime.now().toDate())
-                .value(value)
-                .build();
-    }
-
-    private String getUserLogin()  {
-        return "John Doe"; //SecurityExecutionContext.currentContext().getUserLoginName()
-    }
 }
