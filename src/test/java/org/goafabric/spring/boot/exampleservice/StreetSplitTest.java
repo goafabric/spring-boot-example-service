c6a00b728ad0844b2ff4a2a5fc080e4af17bb21b
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-04-22 14:29:02 +0200

    street split added

--- src/test/java/org/goafabric/spring/boot/exampleservice/StreetSplitTest.java
+++ src/test/java/org/goafabric/spring/boot/exampleservice/StreetSplitTest.java
@@ -13,11 +13,16 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class StreetSplitTest {
     @Test
     public void test() {
-        assertThat(splitStreet("Fichtenstraße 10a").group(1)).isEqualTo("Fichtenstraße");
-        assertThat(splitStreet("Fichtenstraße 10a").group(2)).isEqualTo("10a");
+        assertThat(splitStreet("Fichtenstraße 10-23a").group(1)).isEqualTo("Fichtenstraße");
+        assertThat(splitStreet("Fichtenstraße 10-23a").group(2)).isEqualTo("10-23a");
 
         assertThat(splitStreet("am Kürchen 5").group(1)).isEqualTo("am Kürchen");
         assertThat(splitStreet("am Kürchen 5").group(2)).isEqualTo("5");
+
+        assertThat(splitStreet("Name der Strase 25a").group(1)).isEqualTo("Name der Strase");
+        assertThat(splitStreet("Name der Strase 25a").group(2)).isEqualTo("25a");
+
+        //splitStreet("Straße des 17. Juni 23");
     }
 
 
@@ -25,6 +30,7 @@ public class StreetSplitTest {
         final Pattern pattern = Pattern.compile("([ a-zA-zäöüß]+) ([\\w]+)");
         final Matcher matcher = pattern.matcher(street);
         if (!matcher.find() || matcher.groupCount() != 2) throw new IllegalStateException("street could not be split: " + street);
+        log.info(matcher.group(1));log.info(matcher.group(2));
         return matcher;
     }
 }
