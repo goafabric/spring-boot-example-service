95abe6f0584e844f7393eb1fa6e65ddef0481b27
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-12 13:34:17 +0200

    refactoring

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -11,7 +11,3 @@ create table country
 	secret varchar(2048), -- The field has to be much bigger because it has to hold encrypted info
 	version bigint default 0
 );
-
-INSERT INTO country (id, isocode, name, information, secret) values('1', 'de', 'Germany', 'North Europe', null);
-INSERT INTO country (id, isocode, name, information, secret) values('2', 'es', 'Spain', 'West Europe', null);
-INSERT INTO country (id, isocode, name, information, secret) values('3', 'it', 'Italy', 'South Europe', null);
