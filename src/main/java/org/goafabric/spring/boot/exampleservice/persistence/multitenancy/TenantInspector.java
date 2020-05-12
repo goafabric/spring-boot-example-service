3045f55cf74816044d3504b90cadfd6f631e03a1
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-12 18:51:17 +0200

    tenantinspector added

--- src/main/java/org/goafabric/spring/boot/exampleservice/persistence/multitenancy/TenantInspector.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/persistence/multitenancy/TenantInspector.java
@@ -6,7 +6,7 @@ import org.hibernate.resource.jdbc.spi.StatementInspector;
 @Slf4j
 public class TenantInspector implements StatementInspector {
     @Override
-    public String inspect(String sql) { //inspect sqls from entities annotated with @WHERE
+    public String inspect(String sql) { //inspect sqls from entities annotated with @WHERE(".tenantId = '%TENANT_ID%'")
         if (sql.contains(".tenantId = '%TENANT_ID%'")) {
             sql = sql.replace(".tenantId = '%TENANT_ID%'", ".tenantId = '" + TenantIdStorage.getTenantId() + "'");
         }
