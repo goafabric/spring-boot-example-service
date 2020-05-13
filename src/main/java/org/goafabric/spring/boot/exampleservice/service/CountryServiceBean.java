ca24fd55d6895f9e773e13e5b1de4f91d1a40b6b
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-13 17:00:02 +0200

    count added

--- src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryServiceBean.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/service/CountryServiceBean.java
@@ -1,11 +1,10 @@
 package org.goafabric.spring.boot.exampleservice.service;
 
 import lombok.experimental.Delegate;
-import org.goafabric.spring.boot.exampleservice.crossfunctional.DurationLog;
 import org.goafabric.spring.boot.exampleservice.logic.CountryLogic;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by andreas.mautsch on 08.06.2018.
