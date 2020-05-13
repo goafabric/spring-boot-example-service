b8b2b7e55db1f55ea5611301ec90b24deaab9de6
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-13 14:46:08 +0200

    tenantinspector added

--- src/deploy/demodata/V1000__demo_data.sql
+++ src/deploy/demodata/V1000__demo_data.sql
@@ -1,3 +1,3 @@
-INSERT INTO country (id, isocode, name, information, secret) values('1', 'de', 'Germany', 'North Europe', null);
-INSERT INTO country (id, isocode, name, information, secret) values('2', 'es', 'Spain', 'West Europe', null);
-INSERT INTO country (id, isocode, name, information, secret) values('3', 'it', 'Italy', 'South Europe', null);
+INSERT INTO country (id, tenantid, isocode, name, information, secret) values('1', '10', 'de', 'Germany', 'North Europe', null);
+INSERT INTO country (id, tenantid, isocode, name, information, secret) values('2', '10','es', 'Spain', 'West Europe', null);
+INSERT INTO country (id, tenantid,isocode, name, information, secret) values('3', '10','it', 'Italy', 'South Europe', null);
