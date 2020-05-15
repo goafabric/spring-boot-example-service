42185a4dfd513e3390f710d2e97211813f8ddf85
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-05-15 09:27:25 +0200

    TenantId added to Swagger

--- src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SwaggerConfiguration.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SwaggerConfiguration.java
@@ -4,11 +4,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.schema.ModelRef;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableSwagger2
@@ -18,7 +22,15 @@ public class SwaggerConfiguration {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .select()
+                .globalOperationParameters(
+                        Arrays.asList(new ParameterBuilder()
+                                .name("X-TenantId")
+                                .description("TenantId")
+                                .modelRef(new ModelRef("string"))
+                                .parameterType("header")
+                                .defaultValue("10")
+                                .required(true)
+                                .build()))                .select()
                 .apis(RequestHandlerSelectors.basePackage("org.goafabric"))
                 .paths(PathSelectors.any())
                 .build();
