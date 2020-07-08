0465189756c0bff12819f81e89587fe7c67a9bdd
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-07-08 12:07:18 +0200

    DatabaseProvisioning updated

--- src/main/java/org/goafabric/spring/boot/exampleservice/persistence/DatabaseProvisioning.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/persistence/DatabaseProvisioning.java
@@ -9,6 +9,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -24,34 +25,38 @@ public class DatabaseProvisioning implements CommandLineRunner {
     @Value("${database.provisioning.goals:-migrate}")
     private String goals;
 
-    @Bean
-    public FlywayMigrationStrategy flywayMigrationStrategy() {
-        return flyway -> {
-            flyway = configureDemoDataImport(flyway);
-            migrate(flyway);
-        };
+    @Configuration
+    class FlywayConfiguration {
+        @Bean
+        public FlywayMigrationStrategy flywayMigrationStrategy() {
+            return flyway -> {
+                flyway = configureDemoDataImport(flyway);
+                migrate(flyway);
+            };
+        }
+
+        private Flyway configureDemoDataImport(Flyway flyway) {
+            if (goals.contains("-import-demo-data")) {
+                log.info("importing demo data");
+                flyway = Flyway.configure().configuration(flyway.getConfiguration())
+                        .locations("classpath:db/migration", demoDataLocation).load();
+            }
+            return flyway;
+        }
+
+        private void migrate(Flyway flyway) {
+            if (goals.contains("-migrate")) {
+                log.info("calling flyway migration");
+                flyway.migrate();
+            }
+        }
     }
+
 
     @Override
     public void run(String... args) {
         importCatalogData(goals);
         terminate(goals);
-    }
-
-    private void migrate(Flyway flyway) {
-        if (goals.contains("-migrate")) {
-            log.info("calling flyway migration");
-            flyway.migrate();
-        }
-    }
-
-    private Flyway configureDemoDataImport(Flyway flyway) {
-        if (goals.contains("-import-demo-data")) {
-            log.info("importing demo data");
-            flyway = Flyway.configure().configuration(flyway.getConfiguration())
-                    .locations("classpath:db/migration", demoDataLocation).load();
-        }
-        return flyway;
     }
 
     private void importCatalogData(String goals) {
