b18e691aedbd6a2e043f00a997c76fae0dd7c20c
Mautsch, Andreas <andreas.mautsch@cgm.com>
2020-10-05 17:34:48 +0200

    swagger updated to 3.0

--- src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SwaggerConfiguration.java
+++ src/main/java/org/goafabric/spring/boot/exampleservice/configuration/SwaggerConfiguration.java
@@ -3,6 +3,7 @@ package org.goafabric.spring.boot.exampleservice.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -15,7 +16,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
 
 @Configuration
-@EnableSwagger2
 @Import(BeanValidatorPluginsConfiguration.class)
 public class SwaggerConfiguration {
 
