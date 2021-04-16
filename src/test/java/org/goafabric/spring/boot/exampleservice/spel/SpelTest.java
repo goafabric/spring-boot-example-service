7e2f13283a4186f88c78c9a800f90e3fa647a0e5
Andreas Mautsch <amautsch>
2021-04-16 15:52:14 +0200

    stuff changed

--- src/test/java/org/goafabric/spring/boot/exampleservice/spel/SpelTest.java
+++ src/test/java/org/goafabric/spring/boot/exampleservice/spel/SpelTest.java
@@ -2,18 +2,21 @@ package org.goafabric.spring.boot.exampleservice.spel;
 
 import org.junit.Test;
 import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 //@RunWith(SpringRunner.class)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SpelTest {
-    
+
+    private static final ExpressionParser expressionParser = new SpelExpressionParser();
     @Test
     public void test() {
         //ExpressionParser expressionParser = new SpelExpressionParser();
         //Expression expression = expressionParser.parseExpression("'Any string'");
         //Expression expression = expressionParser.parseExpression("#{@tenantIdBean.getId()}");
-        final Expression expression = new SpelExpressionParser().parseExpression("T(org.goafabric.spring.boot.exampleservice.crossfunctional.TenantRequestContext).getTenantId()");
+
+        final Expression expression = expressionParser.parseExpression("T(org.goafabric.spring.boot.exampleservice.crossfunctional.TenantRequestContext).getTenantId()");
         System.out.println("result is " + (String) expression.getValue());
         System.out.println("result is " + (String) expression.getValue());
         System.out.println("result is " + (String) expression.getValue());
