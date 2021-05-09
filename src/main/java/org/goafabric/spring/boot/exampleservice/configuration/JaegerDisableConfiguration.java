35e37ec1f2c035d1b4b759e071182baa3160afd8
Andreas Mautsch <amautsch>
2021-05-09 11:00:42 +0200

    stuff changed

---
+++ src/main/java/org/goafabric/spring/boot/exampleservice/configuration/JaegerDisableConfiguration.java
@@ -0,0 +1,19 @@
+package org.goafabric.spring.boot.exampleservice.configuration;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@ConditionalOnProperty(value = "opentracing.jaeger.disabled", havingValue = "true", matchIfMissing = true)
+@Configuration
+@Slf4j
+public class JaegerDisableConfiguration {
+
+    @Bean
+    public io.opentracing.Tracer jaegerTracer() {
+        log.info("Jaeger disabled");
+        return io.opentracing.noop.NoopTracerFactory.create();
+    }
+}
